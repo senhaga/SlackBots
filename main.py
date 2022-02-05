46da40a90e0e040b061f8ad598654316de3c653e
Mateus R. Lima <87827210+senhaga@users.noreply.github.com>
2022-02-05 02:22:08 -0300

    Bettering of the nord
    Uthrëd can now be prompted into writing rotations (dictionary with required info) into a txt file (JSON format), recover them, check if today is the rotation day and return the usergroup's users id's. He is getting smarter.

--- main.py
+++ main.py
@@ -2,8 +2,13 @@ import slack
 import os
 from pathlib import Path
 from dotenv import load_dotenv
-from flask import Flask
+from flask import Flask, request, Response
 from slackeventsapi import SlackEventAdapter
+import schedule
+import time
+import json
+import datetime
+
 
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
@@ -16,16 +21,77 @@ client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
 
 BOT_ID = client.api_call("auth.test")['user_id']
 
-@slack_event_adapter.on('message')
-def message(payload):
-    event = payload.get('event', {})
-    channel_id = event.get('channel')
-    user_id = event.get ('user')
-    user_text = event.get ('text')
 
-    if BOT_ID != user_id:
-        client.chat_postMessage(channel = channel_id, text = user_text)
+def rotate():
+    listDict=[]
+    doc = open("currentRotations.txt", "r")
+    for line in doc:
+        listDict.append(json.loads(line))
+    #carrega as linhas em formato dicionário json do .txt numa lista de dicionários python
+    for rotation in listDict:
+        if int(rotation['weekDay']) == int(datetime.datetime.today().weekday()): #se hoje bate com a data, execute a rotação
+            print (client.usergroups_users_list(token=os.environ['SLACK_TOKEN'], usergroup=rotation['groupID']))
 
+
+
+
+@app.route('/new-rotation', methods=['POST'])
+def new_rotation ():
+    data = request.form
+    channel_id = data.get('channel_id')
+    channel_name = data.get('channel_name')
+
+    rawInput = data.get('text')
+    listInput = rawInput.lower().split()
+
+    toUpdate = listInput[0].strip('*')
+    toUpdateID = ''
+    usrGroups = client.usergroups_list(token=os.environ['SLACK_TOKEN']).data['usergroups']
+    for group in usrGroups:
+        if toUpdate==group['handle']:
+            toUpdateID = group['id']
+    #getting the group's ID 
+
+    if toUpdateID!='':
+        rotDict={}
+        rotDict["groupID"] = toUpdateID
+        rotDict["groupHandle"] = listInput[0]
+        rotDict["weekDay"]=listInput[1] #monday=0, sunday=6   
+        rotDict["cicleDuration"] = listInput[2]
+        rotDict["listPerm"] = listInput[4:4+int(listInput [3])]
+        rotDict["listRot"] = listInput[4+int(listInput [3]):]
+   
+
+    jsonDict = json.dumps(rotDict) 
+
+    doc = open("currentRotations.txt", "a")
+    doc.write(str(jsonDict))
+    doc.close()
+
+    rotate()
+  
+    #/new-rotation legal-eng_lawsuits-oncall
+    return Response(), 200
+
+
+
+@app.route('/new-oncall', methods=['POST'])
+def new_oncall ():
+    data = request.form
+    channel_id = data.get('channel_id')
+    channel_name = data.get('channel_name')
+
+    rawInput = data.get('text')
+    listInput = rawInput.split()
+    newOncallName = listInput[0]
+    listInput.pop(0)
+
+    print (data)
+
+    client.usergroups_create(name=newOncallName, token=os.environ['SLACK_TOKEN'])
+
+    return Response(), 200
+  
 
 
 
@@ -34,5 +100,7 @@ if __name__ == "__main__":
 #automaticaly reruns webserver
 
 
-def createOncallRotation (userGroupName, weekdays):
-    pass
+
+
+
+#schedule.every().day.at("01:00").do(job,'It is 01:00')
